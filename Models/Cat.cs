a2556072f2c48a75980fe63908ce855a3b76872e
Maria Jose Montes de Oca <maria.montes@arkanosoft.com>
2018-08-19 18:45:36 -0300

    Cambios en clase cat

--- Models/Cat.cs
+++ Models/Cat.cs
@@ -3,5 +3,9 @@
     public class Cat
     {
         public string Name { get; set; }
-    }
+        public static string description = "Animales que se manejar en el hogar, comen y duermen mucho"
+
+        private Cat() {
+        }
+        
 }
