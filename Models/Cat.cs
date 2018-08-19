3d4125da1f6d538c15654f7881deb025c3d516dc
Maria Jose Montes de Oca <maria.montes@arkanosoft.com>
2018-08-19 18:48:45 -0300

    commit

--- Models/Cat.cs
+++ Models/Cat.cs
@@ -3,7 +3,7 @@
     public class Cat
     {
         public string Name { get; set; }
-        public static string description = "Animales que se manejar en el hogar, comen y duermen mucho"
+        public static string description = "Animales que se manejar en el hogar, comen y duermen mucho";
 
         private Cat() {
         }
