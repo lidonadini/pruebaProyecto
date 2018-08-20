c7af2d6ffe229ed1335e0c528721d00ca70225d9
Maria Jose Montes de Oca <maria.montes@arkanosoft.com>
2018-08-19 23:22:25 -0300

    bueno aca va la solucion de angular

--- Models/Cat.cs
+++ Models/Cat.cs
@@ -3,9 +3,8 @@
     public class Cat
     {
         public string Name { get; set; }
-        public static string description = "Animales que se manejar en el hogar, comen y duermen mucho";
 
-        private Cat() {
-        }
+        private Cat() { }
+    }
         
 }
