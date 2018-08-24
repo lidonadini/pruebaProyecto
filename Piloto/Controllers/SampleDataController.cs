b6052b237fe6b7073c3697e9fcccdc1d2f728d49
Daniela Andrade <dandra12011995@gmail.com>
2018-08-23 21:01:34 -0300

    Prueba

--- Piloto/Controllers/SampleDataController.cs
+++ Piloto/Controllers/SampleDataController.cs
@@ -17,6 +17,7 @@ namespace Piloto.Controllers
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
